f202c86a8f8c3595b15b1ed934e40520504066ce
FIX94 <fix94.1@gmail.com>
2017-01-06 20:16:48 +0100

    oh yea

--- wiiu/src/main.c
+++ wiiu/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 FIX94
+ * Copyright (C) 2016-2017 FIX94
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
